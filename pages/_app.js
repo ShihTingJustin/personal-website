f6637f3642a72d673b3a6b740afc404f7f896395
ShihTingJustin <justinhuang777@gmail.com>
2021-09-20 00:44:17 +0800

    remove sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
+import React, { useEffect } from "react";
+import "../styles/globals.css";
+import "animate.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+      AOS.init();
+    }, []);
   return <Component {...pageProps} />
 }
 
