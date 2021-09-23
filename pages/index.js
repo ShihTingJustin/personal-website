b11dad149ecdd7530d27bf544d8afbf090c3d18c
Justin Huang <justinhuang777@gmail.com>
2021-09-24 01:31:26 +0800

    add outline and style

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,12 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import linkedinIcon from "../public/linkedin_icon.svg";
+
 export default function Home() {
   useEffect(() => {
     new Typewriter("#cover-text", {
-      strings: [
-        "Developer",
-        "Programmer",
-        "Learner",
-        "Apprentice",
-        "Product Manager",
-        "Petrol-head",
-      ],
+      strings: ["Developer", "Learner", "TA", "Petrol-head", "Product Manager"],
       autoStart: true,
       loop: true,
       deleteSpeed: 50,
@@ -22,58 +17,50 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
+    <div id="container">
       <div id="cover">
         <div id="cover-text"></div>
       </div>
-      <div className="fixed">
-        <p>1</p>
-        <div className="expand"></div>
+      <div id="intro" className="">
+        <div id="intro-image"></div>
+        <div id="intro-content">
+          <div className="content" data-aos="fade-up">
+            <h2>賈斯丁</h2>
+            <h3>不安分的靈魂</h3>
+            <p></p>
+          </div>
+        </div>
+      </div>
+      <div id="developer" className="fixed">
+        <div className="content" data-aos="fade-up">
+          <h3>Developer</h3>
+          <p></p>
+        </div>
       </div>
 
-      <div className="fixed1">
-        <p>1</p>
-        <div className="expand"></div>
+      <div id="petrolHead" className="fixed">
+        <div className="content" data-aos="fade-up">
+          <h3>汽車愛好者</h3>
+          <p></p>
+        </div>
       </div>
 
-      <div className="item" data-aos="fade-up">
-        1
-      </div>
-      <div className="item" data-aos="fade-down">
-        2
-      </div>
-      <div className="item" data-aos="fade-right">
-        3
-      </div>
-      <div className="item" data-aos="fade-left">
-        4
-      </div>
-      <div className="item" data-aos="fade-up">
-        1
+      <div id="quote" className="fixed">
+        <div className="content" data-aos="fade-up">
+          <h5>
+            "Your time is limited, so don't waste it living someone else's
+            life."
+          </h5>
+          <p>Steve Jobs</p>
+        </div>
       </div>
 
-      <div className="item" data-aos="zoom-in">
-        5
-      </div>
-      <div className="item" data-aos="zoom-out">
-        6
-      </div>
-
-      <div className="item" data-aos="slide-up">
-        7
-      </div>
-
-      <div className="item" data-aos="flip-up">
-        8
-      </div>
-      <div className="item" data-aos="flip-down">
-        9
-      </div>
-      <div className="item" data-aos="flip-right">
-        10
-      </div>
-      <div className="item" data-aos="flip-left">
-        11
+      <div id="social-media" className="fixed">
+        <div className="content" data-aos="fade-up">
+          <div>
+            <Image src={linkedinIcon} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
