09994d75bb6389f699f2bdf587eea2ea75a4d988
ShihTingJustin <justinhuang777@gmail.com>
2021-10-01 00:40:39 +0800

    update intro section

--- pages/index.js
+++ pages/index.js
@@ -51,47 +51,47 @@ export default function Home() {
       <section id="intro" className="">
         <div id="intro-image"></div>
         <div id="intro-content">
-          <div className="content" data-aos="fade-up">
-            <h1>賈斯丁·黃</h1>
+          <div className="title" data-aos="fade-up">
+            <h2>賈斯丁·黃</h2>
             <h3>不安分的靈魂</h3>
           </div>
-          <div data-aos="fade-up">
-            一個非典型技術宅，鑽研技術也享受打造產品和琢磨細節的過程。
-            <br />
-            被敏捷開發燒到後決定前往軟體領域一探究竟，目前在電商產業擔任前端工程師。
+          <div className="content" data-aos="fade-up">
+            一個非典型技術宅，鑽研技術也享受打造產品和琢磨細節的過程，被敏捷開發燒到後決定前往軟體領域一探究竟。
+            <br/>
+            目前在電商產業擔任前端工程師，也在程式教育機構兼職助教。
           </div>
-          <div data-aos="fade-up">
+          <div className="content" data-aos="fade-up">
             汽車愛好者，對德國工藝特別著迷，尤其是賓士全方位的產品配置和保時捷獨樹一格的德式激情。
             <br />
             喜歡分析市場和規格，曾在台灣唯一整車研發企業擔任產品經理。
           </div>
+          <div className="content" data-aos="fade-up">
+            指數化投資的實踐者，經歷美股兩週四次熔斷和後續V型反轉的洗禮後，領悟了一套適合自己的投資哲學。
+          </div>
+          <div className="content" data-aos="fade-up">
+            愛看科幻電影滿足想像力，像是駭客任務、星際大戰、黑暗騎士、全面啟動、普羅米修斯、天能等等;也喜歡昆丁塔倫提諾獨特的暴力美學和敘事手法，像是惡棍特工、決殺令、八惡人、從前有個好萊塢。
+          </div>
         </div>
       </section>
-      <div id="about" className="fixed">
-        <div id="about-content">
-          <p>
-            指數化投資的實踐者，經歷美股兩週四次熔斷和後續V型反轉的洗禮後，領悟了一套適合自己的投資哲學。
-          </p>
-          <p>
-            愛看科幻電影滿足想像力，像是駭客任務、星際大戰、黑暗騎士、全面啟動、普羅米修斯、天能等等。
-          </p>
-        </div>
-      </div>
-      <div id="developer" className="fixed">
+      <section id="developer" className="fixed">
         <div className="bg">
           <div className="content" data-aos="fade-up">
             <h3>Developer</h3>
             <p></p>
           </div>
         </div>
-      </div>
-
-      <div id="petrolHead" className="fixed">
+      </section>
+      <section id="about" className="fixed">
+        <div className="content" data-aos="fade-up">
+          愛看科幻電影滿足想像力，像是駭客任務、星際大戰、黑暗騎士、全面啟動、普羅米修斯、天能等等。
+        </div>
+      </section>
+      <section id="petrolHead" className="fixed">
         <div className="content" data-aos="fade-up">
           <h3>汽車愛好者</h3>
           <p></p>
         </div>
-      </div>
+      </section>
 
       <div id="quote" className="fixed">
         <div className="content" data-aos="fade-up">
