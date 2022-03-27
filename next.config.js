a5246fe45b1764f32fcf9bbc47f0cb76692d139c
ShihTingJustin <justinhuang777@gmail.com>
2022-03-27 19:43:04 +0800

    feat: add @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   reactStrictMode: true,
   i18n: {
-    locales: ["zh-tw", "en"],
-    defaultLocale: "zh-tw",
+    locales: ['zh-tw', 'en'],
+    defaultLocale: 'zh-tw',
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        // test: /\.(js|ts)x?$/,
+        // for webpack 5 use
+        and: [/\.(js|ts)x?$/],
+      },
+
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
 };
