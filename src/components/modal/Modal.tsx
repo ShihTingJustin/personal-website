4dc2405d2f328228b3ded0fa8eb26714d23855da
ShihTingJustin <justinhuang777@gmail.com>
2022-04-09 22:34:52 +0800

    refactor: add modal background image bu next/image

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { Modal as ModalBootstrap } from 'react-bootstrap';
+import style from './modal.module.scss';
 
 interface ModalProps {
   bg: string;
@@ -18,6 +19,15 @@ const Modal = ({ bg, isOpen, children, setModalOpen }: ModalProps) => {
       show={isOpen}
       onHide={() => setModalOpen(false)}
     >
+      <Image
+        className={style['modal-bg-image']}
+        priority
+        src="https://images.unsplash.com/photo-1538370965046-79c0d6907d47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"
+        layout="fill"
+        width="300"
+        height="300"
+        alt="pic"
+      />
       <ModalBootstrap.Header closeButton closeVariant="white"></ModalBootstrap.Header>
       <ModalBootstrap.Body>
         <div className="modal-body-content-wrapper">
