ed06beea7b3efb98e77520653cc6a387fe416aa8
ShihTingJustin <justinhuang777@gmail.com>
2022-04-10 22:16:00 +0800

    feat: change background image from url to avif

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -22,7 +22,7 @@ const Modal = ({ bg, isOpen, children, setModalOpen }: ModalProps) => {
       <Image
         className={style['modal-bg-image']}
         priority
-        src="https://images.unsplash.com/photo-1538370965046-79c0d6907d47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"
+        src="/image/space.avif"
         layout="fill"
         width="300"
         height="300"
