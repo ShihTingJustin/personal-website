f22852612c787ca8d6732e9a3d39603056a39f95
ShihTingJustin <justinhuang777@gmail.com>
2022-04-17 00:06:56 +0800

    feat: add timeline data and update style

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -23,6 +23,13 @@ import StorybookIcon from '../../../public/icon/storybook.svg';
 import GitlabIcon from '../../../public/icon/gitlab.svg';
 import AntdIcon from '../../../public/icon/antd.svg';
 import JestIcon from '../../../public/icon/jest.svg';
+import VueIcon from '../../../public/icon/vue.svg';
+import NodeIcon from '../../../public/icon/node.svg';
+import ExpressIcon from '../../../public/icon/express.svg';
+import AwsIcon from '../../../public/icon/aws.svg';
+import AzureIcon from '../../../public/icon/azure.svg';
+import AnsibleIcon from '../../../public/icon/ansible.svg';
+import MochaIcon from '../../../public/icon/mocha.svg';
 
 interface TimelineData {
   companyName: string;
@@ -39,27 +46,33 @@ const mockData: TimelineData[] = [
     url: 'https://www.shoalter.com/',
     title: 'Software Engineer',
     content: (
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          svg: {
-            margin: '2px',
-          },
-        }}
-      >
-        <TsIcon width="40" height="40" viewBox="0 0 512 512" alt="icon" />
-        <ReactIcon style={{ fill: '#1993c0' }} width="40" height="40" alt="icon" />
-        <ReduxIcon width="40" height="40" alt="icon" />
-        <NextIcon style={{ fill: '#fff' }} alt="icon" />
-        <SassIcon width="50" height="40" viewBox="0 0 512 512" alt="icon" />
-        <TailwindIcon width="40" height="40" viewBox="0 0 1000 1000" alt="icon" />
-        <AntdIcon width="40" height="40" alt="icon" />
-        <GatsbyIcon width="40" height="40" alt="icon" />
-        <StorybookIcon width="40" height="40" alt="icon" />
-        <GitlabIcon width="50" height="50" alt="icon" />
-        <JestIcon width="40" height="40" alt="icon" />
-      </Box>
+      <>
+        <p>
+          負責香港電商巨擘 HKTVmall 所有 IT
+          專案的前端項目，參與需求分析、使用者體驗及可行性評估、開發及維護。
+        </p>
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            svg: {
+              margin: '2px',
+            },
+          }}
+        >
+          <TsIcon width="40" height="40" viewBox="0 0 512 512" alt="icon" />
+          <ReactIcon style={{ fill: '#1993c0' }} width="40" height="40" alt="icon" />
+          <ReduxIcon width="40" height="40" alt="icon" />
+          <NextIcon style={{ fill: '#fff' }} alt="icon" />
+          <SassIcon width="50" height="40" viewBox="0 0 512 512" alt="icon" />
+          <TailwindIcon width="40" height="40" viewBox="0 0 1000 1000" alt="icon" />
+          <AntdIcon width="40" height="40" alt="icon" />
+          <GatsbyIcon width="40" height="40" alt="icon" />
+          <StorybookIcon width="40" height="40" alt="icon" />
+          <GitlabIcon width="50" height="50" alt="icon" />
+          <JestIcon width="40" height="40" alt="icon" />
+        </Box>
+      </>
     ),
   },
   {
@@ -67,14 +80,38 @@ const mockData: TimelineData[] = [
     companyName: 'Askey (ASUS Group)',
     url: 'https://www.askey.com.tw/tw/',
     title: 'Software Engineer',
-    content: <div>123</div>,
+    content: (
+      <>
+        <p>
+          使用 Azure Pipeline 及 Ansible 建立 CI pipeline 運行自動化測試，將測試報告儲存至 AWS
+          S3，可透過 AWS CloudFront 從外部存取
+        </p>
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            svg: {
+              margin: '2px',
+            },
+          }}
+        >
+          <VueIcon width="40" height="40" alt="icon" />
+          <NodeIcon width="40" height="40" alt="icon" />
+          <MochaIcon width="40" height="40" alt="icon" />
+          <ExpressIcon style={{ background: '#fff' }} width="40" height="40" alt="icon" />
+          <AwsIcon style={{ background: '#fff' }} width="40" height="40" alt="icon" />
+          <AnsibleIcon style={{ background: '#fff' }} width="40" height="40" alt="icon" />
+          <AzureIcon style={{ background: '#fff' }} width="40" height="40" alt="icon" />
+        </Box>
+      </>
+    ),
   },
   {
     period: '2016/12 - 2019/09',
     companyName: 'HAITEC (Yulon Group)',
     url: 'https://www.foxtronev.com/en/',
     title: 'Product Manager',
-    content: <div>123</div>,
+    content: <p>代表華創研發團隊與裕隆集團納智捷價值鏈相關公司合作進行產品企劃及規格配備提案</p>,
   },
 ];
 
@@ -90,7 +127,7 @@ const Timeline = () => {
           {/* <TimelineOppositeContent>{data.title}</TimelineOppositeContent> */}
           <TimelineSeparator
             sx={{
-              height: '25vh',
+              minHeight: '25vh',
             }}
           >
             <TimelineDot />
@@ -106,7 +143,7 @@ const Timeline = () => {
                 </a>
               </div>
               <div className={style['period']}>{data.period}</div>
-              {data.content}
+              <div className={style['content']}>{data.content}</div>
             </Box>
           </TimelineContent>
         </TimelineItem>
