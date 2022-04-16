4080661c3fc2d9a4fc9b64754ee8562e2eb0614e
ShihTingJustin <justinhuang777@gmail.com>
2022-04-16 20:50:10 +0800

    feat: add timeline structure

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -8,44 +8,64 @@ import {
   TimelineDot,
   TimelineOppositeContent,
 } from '@mui/lab';
+import { Box } from '@mui/material';
+interface TimelineData {
+  companyName?: string;
+  title: string;
+  content: React.ReactNode;
+  type: string;
+}
+
+const mockData: TimelineData[] = [
+  {
+    companyName: 'Shoalter Technology',
+    title: 'Software Engineer',
+    content: <div>123</div>,
+    type: 'job',
+  },
+  {
+    companyName: 'Askey',
+    title: 'Software Engineer',
+    content: <div>123</div>,
+    type: 'job',
+  },
+  {
+    companyName: 'HAITEC',
+    title: 'Product Manager',
+    content: <div>123</div>,
+    type: 'job',
+  },
+];
 
 const Timeline = () => {
   return (
-    <TimelineMui position="alternate">
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">09:30 am</TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent data-aos="fade-left">
-          <div>Eat</div>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">10:00 am</TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent data-aos="fade-right">Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">12:00 am</TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">9:00 am</TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Repeat</TimelineContent>
-      </TimelineItem>
+    <TimelineMui
+      position="alternate"
+      // sx={{ marginTop: '-10vw' }}
+    >
+      {mockData.map((data, index) => (
+        <TimelineItem key={index}>
+          {/* RWD */}
+          {/* <TimelineOppositeContent>{data.title}</TimelineOppositeContent> */}
+          <TimelineSeparator
+            sx={
+              {
+                height: '25vh',
+              }
+            }
+          >
+            <TimelineDot />
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent data-aos={`${index % 2 ? 'fade-right' : 'fade-left'}`}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', }}>
+              <div>{data.companyName}</div>
+              <div>{data.title}</div>
+              {data.content}
+            </Box>
+          </TimelineContent>
+        </TimelineItem>
+      ))}
     </TimelineMui>
   );
 };
