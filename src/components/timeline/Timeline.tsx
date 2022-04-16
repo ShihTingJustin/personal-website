c7434da0701b103b5cccd20b820b496255c7fb9d
ShihTingJustin <justinhuang777@gmail.com>
2022-04-16 17:38:08 +0800

    feat: add Timeline module

---
+++ src/components/timeline/Timeline.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  Timeline as TimelineMui,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineOppositeContent,
+} from '@mui/lab';
+
+const Timeline = () => {
+  return (
+    <TimelineMui position="alternate">
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">09:30 am</TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Eat</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">10:00 am</TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">12:00 am</TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Sleep</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">9:00 am</TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Repeat</TimelineContent>
+      </TimelineItem>
+    </TimelineMui>
+  );
+};
+
+export default Timeline;
