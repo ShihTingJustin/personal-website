a6b62131b094a6ca69f9e6437b4b6d65ced78363
ShihTingJustin <justinhuang777@gmail.com>
2022-04-17 18:29:32 +0800

    refactor: extract getIcon function

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -16,25 +16,7 @@ import theme from '../../../src/theme';
 import LinkIcon from '@mui/icons-material/Link';
 import style from './timeline.module.scss';
 
-import ReactIcon from '../../../public/icon/react.svg';
-import ReduxIcon from '../../../public/icon/redux.svg';
-import NextIcon from '../../../public/icon/next.svg';
-import TsIcon from '../../../public/icon/typescript.svg';
-import SassIcon from '../../../public/icon/sass.svg';
-import TailwindIcon from '../../../public/icon/tailwind.svg';
-import GatsbyIcon from '../../../public/icon/gatsby.svg';
-import StorybookIcon from '../../../public/icon/storybook.svg';
-import GitlabIcon from '../../../public/icon/gitlab.svg';
-import AntdIcon from '../../../public/icon/antd.svg';
-import JestIcon from '../../../public/icon/jest.svg';
-import JsIcon from '../../../public/icon/javascript.svg';
-import VueIcon from '../../../public/icon/vue.svg';
-import NodeIcon from '../../../public/icon/node.svg';
-import ExpressIcon from '../../../public/icon/express.svg';
-import AwsIcon from '../../../public/icon/aws.svg';
-import AzureIcon from '../../../public/icon/azure.svg';
-import AnsibleIcon from '../../../public/icon/ansible.svg';
-import MochaIcon from '../../../public/icon/mocha.svg';
+import { getIcon } from '../../utils';
 
 interface TimelineData {
   title: string;
@@ -84,51 +66,6 @@ const mockData: TimelineData[] = [
     content: '代表華創研發團隊與裕隆集團納智捷價值鏈相關公司合作進行產品企劃及規格配備提案。',
   },
 ];
-
-const getIcon = (icon: string) => {
-  switch (icon) {
-    case 'typescript':
-      return <TsIcon width="40" height="40" viewBox="0 0 512 512" alt="icon" />;
-    case 'react':
-      return <ReactIcon style={{ fill: '#1993c0' }} width="40" height="40" alt="icon" />;
-    case 'redux':
-      return <ReduxIcon width="40" height="40" alt="icon" />;
-    case 'next':
-      return <NextIcon style={{ fill: '#fff' }} alt="icon" />;
-    case 'sass':
-      return <SassIcon width="50" height="40" viewBox="0 0 512 512" alt="icon" />;
-    case 'tailwind':
-      return <TailwindIcon width="40" height="40" viewBox="0 0 1000 1000" alt="icon" />;
-    case 'antd':
-      return <AntdIcon width="40" height="40" alt="icon" />;
-    case 'gatsby':
-      return <GatsbyIcon width="40" height="40" alt="icon" />;
-    case 'storybook':
-      return <StorybookIcon width="40" height="40" alt="icon" />;
-    case 'gitlab':
-      return <GitlabIcon width="50" height="50" alt="icon" />;
-    case 'jest':
-      return <JestIcon width="40" height="40" alt="icon" />;
-    case 'javascript':
-      return <JsIcon width="40" height="40" alt="icon" />;
-    case 'vue':
-      return <VueIcon width="40" height="40" alt="icon" />;
-    case 'node':
-      return <NodeIcon width="40" height="40" alt="icon" />;
-    case 'express':
-      return <ExpressIcon style={{ background: '#fff' }} width="40" height="40" alt="icon" />;
-    case 'aws':
-      return <AwsIcon style={{ background: '#fff' }} width="40" height="40" alt="icon" />;
-    case 'azure':
-      return <AzureIcon style={{ background: '#fff' }} width="40" height="40" alt="icon" />;
-    case 'ansible':
-      return <AnsibleIcon style={{ background: '#fff' }} width="40" height="40" alt="icon" />;
-    case 'mocha':
-      return <MochaIcon width="40" height="40" alt="icon" />;
-    default:
-      return;
-  }
-};
 
 const Timeline = () => {
   const matches = useMediaQuery('(max-width:600px)');
