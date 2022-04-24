2f8a31e64bee5dc9ece33fc0d4b228cacc816535
ShihTingJustin <justinhuang777@gmail.com>
2022-04-24 17:42:50 +0800

    feat: add react-router-dom for "scroll to heading"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { useTranslation } from 'react-i18next';
 import Cookies from 'js-cookie';
@@ -110,6 +111,10 @@ export default function Home({
   imgixDomain: string;
   imgixSecureURLToken: string;
 }) {
+  const introRef = useRef(null);
+  const careerRef = useRef(null);
+  const lifeRef = useRef(null);
+  const infoRef = useRef(null);
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
@@ -124,88 +129,104 @@ export default function Home({
   }, [i18n.languages]);
 
   return (
-    <div id="container">
-      <div id="menu-button">
-        <IconButton />
-      </div>
-      <div id="cover" className="block">
-        <div id="cover-text">
-          <Typewriter
-            options={{
-              strings: [
-                'Developer',
-                'Product Manager',
-                'Petrol-head',
-                'Investor',
-                'Teaching Assistant',
-                'Learner',
-              ],
-              autoStart: true,
-              loop: true,
-              delay: 50,
-              deleteSpeed: 25,
-            }}
-          />
-        </div>
-      </div>
-      <div id="intro-wrapper" className="block">
-        <Typography
-          variant="h1"
-          sx={{
-            textAlign: 'center',
-            width: '100%',
-            fontSize: { xs: '4em', sm: '5em', md: '10em' },
-            pb: '0.5em',
-          }}
-        >
-          {t('1_intro_title')}
-        </Typography>
-        <Intro
-          imageLoader={getCustomImageLoader({
-            imgixDomain,
-            imgixSecureURLToken,
-          })}
-        />
-      </div>
-      <div id="timeline-wrapper">
-        <Typography
-          variant="h1"
-          sx={{
-            textAlign: 'center',
-            width: '100%',
-            fontSize: { xs: '4em', sm: '5em', md: '10em' },
-            pb: '0.5em',
-          }}
-        >
-          {t('1_timeline_title')}
-        </Typography>
-        <Timeline />
-      </div>
-      <div id="image-list">
-        <Typography
-          variant="h1"
-          sx={{
-            mt: { xs: '2em' },
-            textAlign: 'center',
-            width: '100%',
-            fontSize: { xs: '4em', sm: '5em', md: '10em' },
-            pb: '0.5em',
-          }}
-        >
-          {t('1_life_title')}
-        </Typography>
-        <ImageList
-          imageLoader={getCustomImageLoader({
-            imgixDomain,
-            imgixSecureURLToken,
-            imgixParams: { fit: 'crop', ar: '1:1' },
-          })}
-        />
-      </div>
-      <div id="further-info" className="block">
-        <FurtherInfo />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div id="container">
+              <div id="menu-button">
+                <IconButton
+                  refs={{
+                    intro: introRef,
+                    career: careerRef,
+                    life: lifeRef,
+                    info: infoRef,
+                  }}
+                />
+              </div>
+              <div id="cover" className="block">
+                <div id="cover-text">
+                  <Typewriter
+                    options={{
+                      strings: [
+                        'Developer',
+                        'Product Manager',
+                        'Petrol-head',
+                        'Investor',
+                        'Teaching Assistant',
+                        'Learner',
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      delay: 50,
+                      deleteSpeed: 25,
+                    }}
+                  />
+                </div>
+              </div>
+              <div ref={introRef} id="intro-wrapper" className="block">
+                <Typography
+                  variant="h1"
+                  sx={{
+                    textAlign: 'center',
+                    width: '100%',
+                    fontSize: { xs: '4em', sm: '5em', md: '10em' },
+                    pb: '0.5em',
+                  }}
+                >
+                  {t('1_intro_title')}
+                </Typography>
+                <Intro
+                  imageLoader={getCustomImageLoader({
+                    imgixDomain,
+                    imgixSecureURLToken,
+                  })}
+                />
+              </div>
+              <div ref={careerRef} id="timeline-wrapper">
+                <Typography
+                  variant="h1"
+                  sx={{
+                    textAlign: 'center',
+                    width: '100%',
+                    fontSize: { xs: '4em', sm: '5em', md: '10em' },
+                    pb: '0.5em',
+                  }}
+                >
+                  {t('1_timeline_title')}
+                </Typography>
+                <Timeline />
+              </div>
+              <div ref={lifeRef} id="image-list">
+                <Typography
+                  variant="h1"
+                  sx={{
+                    mt: { xs: '2em' },
+                    textAlign: 'center',
+                    width: '100%',
+                    fontSize: { xs: '4em', sm: '5em', md: '10em' },
+                    pb: '0.5em',
+                  }}
+                >
+                  {t('1_life_title')}
+                </Typography>
+                <ImageList
+                  imageLoader={getCustomImageLoader({
+                    imgixDomain,
+                    imgixSecureURLToken,
+                    imgixParams: { fit: 'crop', ar: '1:1' },
+                  })}
+                />
+              </div>
+              <div ref={infoRef} id="further-info" className="block">
+                <FurtherInfo />
+              </div>
+            </div>
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 export async function getStaticProps() {
--- src/components/iconButton/IconButton.tsx
+++ src/components/iconButton/IconButton.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import {
   IconButton as IconButtonMui,
@@ -11,9 +12,13 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Switch from '@/Components/switch/Switch';
 import SegmentIcon from '@mui/icons-material/Segment';
 
-export default function IconButton() {
+interface Refs {
+  [key: string]: React.MutableRefObject<HTMLElement | null>;
+}
+
+export default function IconButton({ refs }: { refs: Refs }) {
   const { t, i18n } = useTranslation();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const open = !!anchorEl;
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -21,6 +26,17 @@ export default function IconButton() {
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const handleScroll = (ref: HTMLElement | null) => {
+    if (ref)
+      window.scrollTo({
+        top: ref.offsetTop,
+        left: 0,
+        behavior: 'smooth',
+      });
+  };
+
+  console.log(refs);
 
   return (
     <>
@@ -66,30 +82,38 @@ export default function IconButton() {
         transformOrigin={{ horizontal: 'right', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-        <MenuItemMui>
-          <ListItemIconMui sx={{ fontSize: '1em' }}>
-            <SegmentIcon sx={{ mr: 1 }} fontSize="small" />
-            {t('1_intro_title')}
-          </ListItemIconMui>
-        </MenuItemMui>
-        <MenuItemMui>
-          <ListItemIconMui>
-            <SegmentIcon sx={{ mr: 1 }} fontSize="small" />
-            {t('1_timeline_title')}
-          </ListItemIconMui>
-        </MenuItemMui>
-        <MenuItemMui>
-          <ListItemIconMui>
-            <SegmentIcon sx={{ mr: 1 }} fontSize="small" />
-            {t('1_life_title')}
-          </ListItemIconMui>
-        </MenuItemMui>
-        <MenuItemMui>
-          <ListItemIconMui>
-            <SegmentIcon sx={{ mr: 1 }} fontSize="small" />
-            {t('1_further_info')}
-          </ListItemIconMui>
-        </MenuItemMui>
+        <Link to="/#intro" onClick={() => handleScroll(refs.intro.current)}>
+          <MenuItemMui>
+            <ListItemIconMui sx={{ fontSize: '1em', w: '100%' }}>
+              <SegmentIcon sx={{ mr: 1 }} fontSize="small" />
+              {t('1_intro_title')}
+            </ListItemIconMui>
+          </MenuItemMui>
+        </Link>
+        <Link to="/#career" onClick={() => handleScroll(refs.career.current)}>
+          <MenuItemMui>
+            <ListItemIconMui>
+              <SegmentIcon sx={{ mr: 1 }} fontSize="small" />
+              {t('1_timeline_title')}
+            </ListItemIconMui>
+          </MenuItemMui>
+        </Link>
+        <Link to="/#life" onClick={() => handleScroll(refs.life.current)}>
+          <MenuItemMui>
+            <ListItemIconMui>
+              <SegmentIcon sx={{ mr: 1 }} fontSize="small" />
+              {t('1_life_title')}
+            </ListItemIconMui>
+          </MenuItemMui>
+        </Link>
+        <Link to="/#info" onClick={() => handleScroll(refs.info.current)}>
+          <MenuItemMui>
+            <ListItemIconMui>
+              <SegmentIcon sx={{ mr: 1 }} fontSize="small" />
+              {t('1_further_info')}
+            </ListItemIconMui>
+          </MenuItemMui>
+        </Link>
         <DividerMui />
         <MenuItemMui>
           <ListItemIconMui sx={{ justifyContent: 'center' }}>
