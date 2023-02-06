9caa3d8026b78b20dcccaf040620dd202cbd9876
ShihTingJustin <justinhuang777@gmail.com>
2023-02-06 21:59:06 +0800

    feat: apply new color

--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const cssVar = {
   textGlow: '0 0 80px rgb(192 219 255 / 75%), 0 0 32px rgb(65 120 255 / 24%)',
   web3Gradient: 'linear-gradient(to right, #30cfd0, #c43ad6)',
-  web3Background:
-    'radial-gradient(circle closest-corner at -5% 25%, rgba(40, 72, 122, 0.85), transparent), radial-gradient(circle closest-corner at 120% 60%, rgba(138, 41, 129, 0.7),transparent)',
 };
 
 function MyApp({ Component, pageProps }) {
